010d2a3a9ec7726eb200c27a10788d7f82183e87
Mithun Jothiravi <mithun.jothiravi@mail.utoronto.ca>
2020-05-14 19:14:19 -0400

    Finished breakdown of the first example.

--- 2-Introduction_to_Operating_Systems/2.1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> /* Used to perform input and output operations in C like scanf() and printf(). */
-#include <stdlib.h> /* Perform standard utility functions like dynamic memory allocation, using functions such as malloc() and calloc(). */
-#include <sys/time.h> /* Provides you with time types */
-#include <assert.h> /* This gives you the assert functionality to test your code. */
-
-int main(int argc, char *argv[]) {
-    printf(argc);
-    printf(argv);
-}
